ba317c42cdf623ed183217237820ac1f613a910b
Incendio_K <96165724+Incendio1@users.noreply.github.com>
2026-09-24 22:06:02 +0800

    Add files via upload

--- models/CGMap.py
+++ models/CGMap.py
@@ -1,132 +1,195 @@
 import numpy as np
 import torch
 import torch.nn.functional as F
-from torch_geometric.nn import MessagePassing
 from torch.nn import Linear, Parameter
+from torch_geometric.nn import MessagePassing
 
 
 class prop_sum(MessagePassing):
     def __init__(self, args, **kwargs):
-        super(prop_sum, self).__init__(aggr='add', **kwargs)
+        super().__init__(aggr='add', **kwargs)
         self.args = args
-        self.layers = args.layers
-        self.beta = 0.1
-        self.K = len(args.layers)
-        Gamma = None
+        self.layers = list(args.layers)
+        self.K = len(self.layers)
+        self.beta = float(getattr(args, 'gpr_beta', 0.1))
+        self.init_type = args.Init
+        self.legacy_fixed_self = False
 
-        # assert args.Init in ['PPR', 'NPPR', 'Random', 'WS']
-        if args.Init == 'PPR':
-            TEMP = self.beta * (1 - self.beta) ** np.arange(self.K)
-            TEMP[-1] = (1 - self.beta) ** self.K
-        elif args.Init == 'NPPR':
-            TEMP = (self.beta) ** np.arange(self.K)
-            TEMP = TEMP / np.sum(np.abs(TEMP))
-        elif args.Init == 'Random':
-            bound = np.sqrt(3 / (self.K))
-            TEMP = np.random.uniform(-bound, bound, self.K)
-            TEMP = TEMP / np.sum(np.abs(TEMP))
-        elif args.Init == 'WS':
-            TEMP = Gamma
+        hops = np.asarray(self.layers, dtype=np.float64)
 
-        self.temp = Parameter(torch.tensor(TEMP, dtype=torch.float32))
+        if self.init_type == 'PPR':
+            hop_temp = self.beta * (1.0 - self.beta) ** np.arange(self.K, dtype=np.float64)
+            hop_temp[-1] = (1.0 - self.beta) ** self.K
+            temp = np.concatenate(([1.0], hop_temp))
+            self.legacy_fixed_self = True
+
+        elif self.init_type == 'PPR_raw':
+            hop_temp = self.beta * (1.0 - self.beta) ** hops
+            temp = np.concatenate(([self.beta], hop_temp))
+
+        elif self.init_type == 'SignedPPR':
+            A = float(getattr(args, 'gpr_A', 0.73))
+            r = float(getattr(args, 'gpr_r', 0.67))
+            B = float(getattr(args, 'gpr_B', 0.546))
+            s = float(getattr(args, 'gpr_s', 0.99))
+            positive = A * hops * np.power(r, hops - 1.0)
+            long_range = B * np.power(s, hops - 1.0)
+            hop_temp = positive - long_range
+            temp = np.concatenate(([self.beta], hop_temp))
+
+        elif self.init_type == 'NPPR':
+            exponents = np.concatenate(([0.0], hops))
+            temp = self.beta ** exponents
+            denominator = np.sum(np.abs(temp))
+            if denominator > 0:
+                temp /= denominator
+
+        elif self.init_type == 'Random':
+            bound = np.sqrt(3.0 / (self.K + 1))
+            temp = np.random.uniform(-bound, bound, self.K + 1)
+            denominator = np.sum(np.abs(temp))
+            if denominator > 0:
+                temp /= denominator
+
+        elif self.init_type == 'WS':
+            supplied = getattr(args, 'gpr_weights', None)
+            if supplied is None:
+                raise ValueError('--Init WS requires --gpr_weights.')
+            expected = self.K + 1
+            if len(supplied) != expected:
+                raise ValueError(
+                    f'--gpr_weights must contain {expected} values for layers '
+                    f'{self.layers}; received {len(supplied)}.'
+                )
+            temp = np.asarray(supplied, dtype=np.float64)
+
+        else:
+            raise ValueError(f'Unsupported GPR initialization: {self.init_type}')
+
+        initial = torch.tensor(temp, dtype=torch.float32)
+        self.temp = Parameter(initial.clone(), requires_grad=True)
+        self.register_buffer('_initial_temp', initial.clone())
 
     def forward(self, x, edge_index, edge_weight):
         embed_layer = []
-        embed_layer.append(x)
-        if(self.layers != [0]):
-            for i in range(len(self.layers)):
-                gamma = self.temp[i]
-                h = gamma * self.propagate(edge_index[self.layers[i] - 1], x=x, norm=edge_weight[self.layers[i] - 1])
-                embed_layer.append(h)
 
-        embed_layer = torch.stack(embed_layer, dim=1)
-        return embed_layer
+        if self.legacy_fixed_self:
+            self_channel = x
+        else:
+            self_channel = self.temp[0] * x
+
+        embed_layer.append(self_channel)
+
+        for i, layer in enumerate(self.layers):
+            cache_index = layer - 1
+            weight = self.temp[i + 1]
+            propagated = self.propagate(
+                edge_index[cache_index],
+                x=x,
+                norm=edge_weight[cache_index]
+            )
+            embed_layer.append(weight * propagated)
+
+        return torch.stack(embed_layer, dim=1)
 
     def message(self, x_j, norm):
         return norm.view(-1, 1) * x_j
 
     def reset_parameters(self):
-        for k in range(self.args.K + 1):
-            self.temp.data[k] = self.beta * (1 - self.beta) ** k
-        self.temp.data[-1] = (1 - self.beta) ** self.K
-        self.prop.reset_parameters()
+        with torch.no_grad():
+            self.temp.copy_(self._initial_temp)
 
 
 class prop_weight(MessagePassing):
-    def __init__(self, args, **kwargs):
-        super(prop_weight, self).__init__(aggr='add', **kwargs)
+    """Weighted-sum alternative."""
 
-        self.weight = torch.nn.Parameter(torch.ones(len(args.layers) + 1), requires_grad=True)
-        self.layers = args.layers
-        self.alpha = args.alpha
-        # PPR
-        TEMP = self.beta * (1 - self.beta) ** np.arange(args.K + 1)
-        TEMP[-1] = (1 - self.beta) ** args.K
-        self.temp = Parameter(torch.tensor(TEMP))
+    def __init__(self, args, **kwargs):
+        super().__init__(aggr='add', **kwargs)
+        self.layers = list(args.layers)
+        self.alpha = float(args.alpha)
+        self.weight = Parameter(
+            torch.ones(len(self.layers) + 1, dtype=torch.float32),
+            requires_grad=True
+        )
 
     def forward(self, x, edge_index, edge_weight):
         z = x
-        embed_layer = []
-        embed_layer.append(self.weight[0] * x)
+        embed_layer = [self.weight[0] * x]
 
-        for i in range(len(self.layers)):
-            h = (1 - self.alpha) * self.propagate(edge_index[self.layers[i] - 1], x=x, norm=edge_weight[self.layers[i] - 1]) + self.alpha * z
+        for i, layer in enumerate(self.layers):
+            idx = layer - 1
+            propagated = self.propagate(
+                edge_index[idx], x=x, norm=edge_weight[idx]
+            )
+            h = (1.0 - self.alpha) * propagated + self.alpha * z
             embed_layer.append(self.weight[i + 1] * h)
-        embed_layer = torch.stack(embed_layer, dim = 1)
-        return embed_layer
+
+        return torch.stack(embed_layer, dim=1)
 
     def message(self, x_j, norm):
         return norm.view(-1, 1) * x_j
 
     def reset_parameters(self):
-        self.weight = torch.Parameter(torch.ones(len(self.layers) + 1), requires_grad=True)
+        with torch.no_grad():
+            self.weight.fill_(1.0)
 
 
 class CGMap(torch.nn.Module):
     def __init__(self, args):
-        super(CGMap, self).__init__()
+        super().__init__()
         self.args = args
         self.agg = args.agg
-        self.feature_transform = Linear(58, self.args.hidden)
-        self.output_layer = Linear(self.args.hidden, 1)
+
+        in_features = int(getattr(args, 'num_features', 58))
+
+        self.feature_transform = Linear(in_features, args.hidden)
+        self.output_layer = Linear(args.hidden, 1)
+
         if self.agg == 'sum':
             self.propagation = prop_sum(args)
         elif self.agg == 'weighted_sum':
             self.propagation = prop_weight(args)
-        self.feature_groups = [
-            (0, 16),
-            (16, 32),
-            (32, 48),
-            (48, 58)
-        ]
-        if hasattr(args, 'initial_weights') and args.initial_weights is not None:
-            initial_weights = torch.tensor(args.initial_weights)
         else:
-            initial_weights = torch.tensor([0.05, 0.05, 0.6, 2.3])
+            raise ValueError(f'Unsupported aggregation mode: {self.agg}')
 
-        self.group_weights = Parameter(initial_weights, requires_grad=True)
+        self.feature_groups = [(0, 16), (16, 32), (32, 48), (48, 58)]
+
+        initial_weights = getattr(args, 'initial_weights', None)
+        if initial_weights is None:
+            initial_weights = args.i_w
+
+        group_init = torch.tensor(initial_weights, dtype=torch.float32)
+        self.group_weights = Parameter(group_init.clone(), requires_grad=True)
+        self.register_buffer('_initial_group_weights', group_init.clone())
 
     def reset_parameters(self):
         self.feature_transform.reset_parameters()
         self.output_layer.reset_parameters()
 
+        if hasattr(self.propagation, 'reset_parameters'):
+            self.propagation.reset_parameters()
+
+        with torch.no_grad():
+            self.group_weights.copy_(self._initial_group_weights)
+
     def _apply_feature_weighting(self, x):
-        weighted_features = []
-
-        for i, (start_idx, end_idx) in enumerate(self.feature_groups):
-            feature_slice = x[:, start_idx:end_idx]
-            weighted_slice = self.group_weights[i] * feature_slice
-            weighted_features.append(weighted_slice)
-
-        return torch.cat(weighted_features, dim=1)
+        parts = []
+        for i, (start, end) in enumerate(self.feature_groups):
+            parts.append(self.group_weights[i] * x[:, start:end])
+        return torch.cat(parts, dim=1)
 
     def forward(self, data):
-        x, _ = data.x, data.edge_index
-        weighted_features = self._apply_feature_weighting(x)
-        x = F.dropout(weighted_features, p=self.args.dropout, training=self.training)
+        x = self._apply_feature_weighting(data.x)
+
+        x = F.dropout(x, p=self.args.dropout, training=self.training)
         x = F.relu(self.feature_transform(x))
         x = F.dropout(x, p=self.args.dropout, training=self.training)
         x = self.output_layer(x)
-        embed = self.propagation(x, self.args.hop_edge_index, self.args.hop_edge_att)
-        x = torch.sum(embed, dim=1)
-        return x
+
+        embed = self.propagation(
+            x,
+            self.args.hop_edge_index,
+            self.args.hop_edge_att
+        )
+
+        return torch.sum(embed, dim=1)
